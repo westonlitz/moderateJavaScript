b3b84b74887cde3901ab4881d66e2aa4f20dff41
Weston Scott Litz <59563145+westonlitz@users.noreply.github.com>
2024-05-01 10:45:46 -0500

    Update accounts.js

--- Project-_Local_library-_Qualified_assessment_-Node_18--Weston_Litz-Solution/public/src/accounts.js
+++ Project-_Local_library-_Qualified_assessment_-Node_18--Weston_Litz-Solution/public/src/accounts.js
@@ -18,7 +18,7 @@ return books.reduce((total, book) => {
 }
 
 
-function getBooksPossessedByAccount(account, books, authors) {
+/* function getBooksPossessedByAccount(account, books, authors) {
 const accountID = account.id
 return books
   .filter (book => {
@@ -30,6 +30,30 @@ return books
 })
   return borrowers.slice(0, 10)
 }
+/* 
+
+//Let's write the above working function into two functions. A main one and one that helps support that main, tested function:
+
+// Helper function to filter and map books
+function filterAndMapBooks(accountID, books, authors) {
+  return books
+    .filter(book => {
+      const recentBorrow = book.borrows[0];
+      return recentBorrow.id === accountID && !recentBorrow.returned;
+    })
+    .map(book => {
+      const author = authors.find(author => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+// Main function to get books possessed by an account
+function getBooksPossessedByAccount(account, books, authors) {
+  const accountID = account.id;
+  const possessedBooks = filterAndMapBooks(accountID, books, authors);
+  return possessedBooks.slice(0, 10); // Limit the result to 10 books
+}
+
 
 module.exports = {
   findAccountById,
